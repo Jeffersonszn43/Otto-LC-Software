5ae1e97d5a43f24070f7576e1638ca6788135b53
Jeffersonszn43 <xenoblade61097@gmail.com>
2025-07-23 16:19:03 -0400

    Finished updating the code

--- Otto_LC_Software/Otto_LC.py
+++ Otto_LC_Software/Otto_LC.py
@@ -1,4 +1,4 @@
-# Here is the software that will have the control logic for the Otto LC robot.
+# Here is the skeleton code that will have the control logic for your Otto LC robot.
 # Written By: Jefferson Charles
 
 # Here are the libraries needed for the program
@@ -12,13 +12,13 @@ LeftFoot = 12
 RightFoot = 10
 Buzzer = 20
 
-# Here is where we are initializing and defining all of the pins of the sensors on the robot
+# Here is where we are initializing and defining all of the pins of the sensors on your robot
 LDR_Pin = ADC(28)  # ADC pin for the Photoresistor.
 
 Trig_Pin = Pin(22, Pin.OUT) # Trigger pin on Ultrasonic Distance Sensor.
 Echo_Pin = Pin(21, Pin.IN)  # Echo pin on the Ultrasonic Distance Sensor.
 
-# Here we are initializing the 8x8 LED Matrix that will be used for displaying the different emotions of the robot.
+# Here we are initializing the 8x8 LED Matrix that will be used for displaying the different emotions of your robot.
 Din = 19
 SCLK = 18
 CS = 17
@@ -33,7 +33,7 @@ otto.init(3, 7, 12, 10, True, 20, 1, 2, 19)
 otto.initMATRIX(Din, CS, SCLK, Orientation)
 otto.home()
 
-# This function is responsible for the light sensor integration on the robot
+# This function is responsible for the light sensor integration on your robot
 def light_levels():
     sensor_value = LDR_Pin.read_u16()  # Here we are reading the ADC value (0 - 65535)
     
@@ -43,7 +43,7 @@ def light_levels():
     # Here we are computing the deazone that will be 5% from the sensor value of the light sensor
     deadzone = int(sensor_value * 0.05)
     
-    # Here we are observing the light levels of the room Jerry is in
+    # Here we are observing the light levels of the room your robot is in
     if sensor_value >= 56 + deadzone:
         return "dark"
         
@@ -73,7 +73,7 @@ def get_distance ():
     return distance
 
 
-# Function that will allow the robot to implement obstacle avoidance during its movements
+# Function that will allow your robot to implement obstacle avoidance during its movements
 def obstacle_avoidance():
     detected_object = get_distance()
     
@@ -82,7 +82,7 @@ def obstacle_avoidance():
         otto.putMouth(20)
         otto.playGesture(5)
         time.sleep_ms(500)
-        # This will make the robot walk back a little bit and turn once it detects an object in front of it
+        # This will make your robot walk back a little bit and turn once it detects an object in front of it
         otto.walk(2, 1000, -1)
         otto.turn(2, 1000, 1)
         time.sleep_ms(500)
@@ -90,91 +90,90 @@ def obstacle_avoidance():
         print("No object in front of me")
             
 
-# Here are the functions for the main control logic for the robot
+# Here are the functions for the main control logic for your robot that you will have to complete
 
-# This function will be responsible for the movement options the robot can perform, while implementing obstacle avoidance
+# This function will be responsible for the movement options your robot can perform, while implementing obstacle avoidance
 def otto_movements():
-    otto.walk(4, 1000, 1) # Walking forward
-    time.sleep_ms(200)
-    otto.walk(4, 1000, -1) # Walking backwards
-    time.sleep_ms(200)
-    otto.turn(2, 1000, 1) # Turning to the left
-    time.sleep_ms(200)
-    otto.turn(2, 1000, -1) # Turning to the right
-    time.sleep_ms(200)
-    otto.bend(1, 500, 1) # Bending the left foot
-    time.sleep_ms(200)
-    otto.bend(1, 2000, -1) # Bending the right foot
-    time.sleep_ms(200)
-    otto.home() # Makes the robot go to its default position
-    time.sleep_ms(100)
-
-# This function will be responsible for the robot dancing
-def otto_dances():
-    otto.shakeLeg(1, 1500, 1) # Left
-    otto.shakeLeg(1, 2000, -1) # Right
-    otto.moonwalker(4, 1000, 25, 1) # Left
-    otto.moonwalker(4, 1000, 25, -1) # Right
-    otto.crusaito(3, 1000, 20, 1) # Left
-    otto.crusaito(3, 1000, 20, -1) # Right
-    time.sleep_ms(100)
-    otto.flapping(3, 1000, 20, 1) # Left
-    otto.flapping(3, 1000, 20, -1) # Right
-    time.sleep_ms(100)
-    otto.swing(3, 1000, 20)
-    otto.tiptoeSwing(3, 1000, 20)
-    otto.jitter(3, 1000, 20)
-    otto.updown(3, 1500, 20)
-    otto.ascendingTurn(3, 1000, 50)
-    otto.jump(1, 2000)
-    otto.home()
-    time.sleep_ms(100)
-
-# This function is responsible for sounds that the robot can produce
-def otto_sounds():
-    otto.sing(0) # Connection sound
-    otto.sing(1) # Disconnection sound
-    otto.sing(3) # OHOOH sound
-    otto.sing(18) # Buttonpressed sound
-
-# This function is responsible for gestures performed by the robot
-def otto_gestures():
-    otto.playGesture(0) # Happy gesture
-    time.sleep_ms(600)
-    otto.playGesture(2) # Sad gesture
-    time.sleep_ms(600)
-    otto.playGesture(11) # Victory gesture
-    time.sleep_ms(600)
-    otto.playGesture(3) # Sleeping gesture
-    time.sleep_ms(600)
-
-# This function is responsible for displaying the emotions of the robot
-def otto_emotions():
-    otto.putMouth(10) # Smile emotion
-    time.sleep_ms(700)
-    otto.putMouth(11) # Happyopen emotion
-    time.sleep_ms(700)
-    otto.putMouth(22) # Sad emotion
-    time.sleep_ms(700)
-    otto.putMouth(20) # Confused emotion
-    time.sleep_ms(700)    
+    """
+    Enter the code for the movement logic for your robot here using methods like:
+    - walk()
+    - turn() 
     
-# This loop will be responsible for 
+    Note: Make sure to implement a 200 ms delay in between your logic.
+    """
+
+    # Enter your code below:
+
+
+    otto.home() # Makes your robot go to its default position
+    time.sleep_ms(100)
+
+# This function will be responsible for your robot performing dances
+def otto_dances():
+    """
+    Enter the code for the dancing logic for your robot here using methods like: 
+    - moonwalker()
+    - crusaito()
+
+    Note: Make sure to implement a 200 ms delay in between your logic.
+    """
+    
+    # Enter your code below:
+
+
+    otto.home() # Makes your robot go to its default position
+    time.sleep_ms(100)
+
+# This function is responsible for the robotic sounds that your robot can produce
+def otto_sounds():
+    """
+    Enter the code to have your robot produce robotic sounds here using methods like:
+    - sing(#)
+    """
+
+    # Enter your code below:
+
+
+# This function is responsible for the emotional gestures performed by your robot
+def otto_gestures():
+    """
+    Enter the code here to have your robot perform emotional gestures using methods like:
+    - playGesture(#)
+
+    Note: Make sure that you implement a 200 ms delay in between your logic.
+    """
+
+    # Enter your code below:
+
+
+# This function is responsible for displaying the emotions of your robot
+def otto_emotions():
+    """
+    Enter the code here to have your robot display facial expressions on the LED Matrix using methods like:
+    - putMouth(#)
+
+    Note: Make sure to implement a 200 ms delay in between your logic.
+    """   
+
+    # Enter your code below:
+
+    
+# This loop will be responsible for executing what your robot will do depending on the light levels of the room your robot is in
 while True:
-    otto.home() # Sets the robot in its neutral position
+    otto.home() # Sets your robot in its neutral position
     time.sleep(1)
     
-    current_light = light_levels() # Here will be the light sensor values produced by the robot
+    current_light = light_levels() # Here will be the light sensor values produced by your robot
     
-    # If the room the robot is in is dark, then the robot will perform these actions instead
+    # If the room your robot is in is dark, then the robot will perform these actions
     if current_light == "dark":
         otto.putMouth(24) # Sadclosed emotion
         otto.sing(2) # Surprise sound
         otto.playGesture(5) # Confused gesture
         
-    # If there is enough light in the room the robot is currently in, then it will perform its movements    
+    # If the room your robot is in is bright enough, then your robot will perferm these actions instead   
     elif current_light == "bright":
-        # This function call is responsible for the implementation of obstacle avoidance on the robot
+        # This function call is responsible for the implementation of obstacle avoidance on your robot
         obstacle_avoidance()
         
         otto_movements()
